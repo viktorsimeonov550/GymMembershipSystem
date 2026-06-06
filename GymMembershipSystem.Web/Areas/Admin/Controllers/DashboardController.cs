bd5ec2d3946fbb21ea0f672ff3a473c7b74e10de
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2026-06-06 11:42:49 +0300

    Filled in admin controllers pt1

--- GymMembershipSystem.Web/Areas/Admin/Controllers/DashboardController.cs
+++ GymMembershipSystem.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,13 @@
-﻿namespace GymMembershipSystem.Web.Areas.Admin.Controllers
+using GymMembershipSystem.Data.Common;
+using GymMembershipSystem.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace GymMembershipSystem.Web.Areas.Admin.Controllers;
+[Area("Admin")]
+[Authorize(Roles = ApplicationConstants.AdministratorRoleName)]
+public class DashboardController : Controller
 {
-    public class DashboardController
-    {
-    }
+    private readonly IStatisticsService service;
+    public DashboardController(IStatisticsService service) => this.service = service;
+    public async Task<IActionResult> Index() => View(await service.GetDashboardStatisticsAsync());
 }
