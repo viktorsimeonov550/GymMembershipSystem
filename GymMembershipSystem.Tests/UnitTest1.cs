ad3f892d59008dd983b6a01e0c28f78924367a54
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2026-06-09 09:04:37 +0300

    Added project for tests

---
+++ GymMembershipSystem.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GymMembershipSystem.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
